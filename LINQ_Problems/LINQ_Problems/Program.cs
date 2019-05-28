24c81249c3c4bc6a99a392ccefc6594ea391cdb8
Mitchell Mittelstedt <mmittelstedt@icloud.com>
2019-05-28 16:18:26 -0500

    third question, figuring out why OrderBy isn't orderering and RemoveAt isn't removing the lowest value

--- LINQ_Problems/LINQ_Problems/Program.cs
+++ LINQ_Problems/LINQ_Problems/Program.cs
@@ -19,6 +19,65 @@ namespace LINQ_Problems
                 }
             }
             Console.ReadLine();
+
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            var distinctNames = names.Distinct();  //IEnumerable
+            foreach (string name in distinctNames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+
+            var result = classGrades.Select(x => x.Split(',')).ToList();
+            Console.WriteLine(result);
+            foreach (string[] gradeString in result)
+            {
+                var gradeIntList = gradeString.Select(r => int.Parse(r)).ToList();
+                gradeIntList.OrderBy(p => p).ToList().RemoveAt(0);
+                int sumOfGrades = gradeIntList.Sum();
+                int avgOfGrades = sumOfGrades / gradeIntList.Count;
+            }
+
+
+
+        }
+
+
+            //var intResult = result.Select(r => r.Select(q => int.Parse(q)).ToList()).ToList();
+            //Console.WriteLine(intResult);
+            //Console.ReadLine();
+           // intResult.Select(x => x.Select(y => y.OrderByDescending);
+            //foreach (var gradeArray in result)
+
+            //{
+
+                
+                //var numberToRemove = intResult.
+                //foreach (var number in intResult)
+                //{
+                //    List<int> newGrades = new List<int>();
+                //    if (number != numberToRemove)
+                //    {
+                //        newGrades.Add(number);
+                //    }
+                //}
+                //intResult.RemoveAll(x => x==numberToRemove) //
+                //intResult.
+                //}
+
+                //var newResult = gradeArray.Sum()
+           
+                //take in list of strings, drops lowest grade from each new list of strings, averages rest of grades (excluding min), averages averages
+
+
         }
     }
-}
+
